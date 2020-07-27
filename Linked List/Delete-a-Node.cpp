0d79f0f0d2a8e7b89b4edf00c1c1f4eabcc5e329
dhiman-12 <aayush.dhiman7777@gmail.com>
2020-07-27 14:18:31 +0530

    Linked List

---
+++ Linked List/Delete-a-Node.cpp
@@ -0,0 +1,110 @@
+// Delete a node either first node or any node from the Linked List
+
+
+#include<iostream>
+using namespace std;
+struct Node{
+	int data;
+	struct Node*next;
+};
+
+struct Node *head=NULL;
+
+void create(int n)
+{
+	struct Node *p,*temp;
+	int data;
+	head=new Node;
+	if(head==NULL)
+	{
+		cout<<"memeory is not Alloctaed";
+	}
+	else
+	{
+		cin>>data;
+		head->data=data;
+		head->next=NULL;
+		p=head;
+		for(int i=2;i<=n;i++)
+		{
+			if(p==NULL)
+			{
+				cout<<"Memeory is not Alloctaed to head Node";
+				break;
+			}
+			else
+			{
+				temp= new Node;
+				cin>>data;
+				temp->data=data;
+				temp->next=NULL;
+				p->next=temp;
+				p=p->next;
+			}
+		}
+	}
+	
+}
+
+void Delete(int position)
+{
+	int z=1;
+	struct Node*temp,*q;
+	int x;
+	if(position==0)
+	{
+		z=0;
+		temp=head;
+		x=head->data;
+		cout<<"Element Deleted is = "<<x<<"\n";
+		head=head->next;
+		delete temp;       // Deallocate the node from the memeory 
+	}
+	else
+	{
+		temp=head;
+		q=NULL;
+		for(int i=0;i<position-1;i++)
+		{
+			z=0;
+			q=temp;
+			temp=temp->next;
+		}
+		q->next=temp->next;
+		x=temp->data;
+		cout<<"Element Deleted is= "<<x<<"\n";
+		delete temp;   // Deallocate the node from the memeory 
+	}
+	if(z==1)
+	{
+		cout<<"aayush";
+	}
+	
+}
+void display()
+{
+	struct Node *temp;
+	temp=head;
+	while(temp)
+	{
+		cout<<temp->data;
+		temp=temp->next;
+	}
+}
+
+int main()
+{
+	int n,position;
+	cin>>n;
+	cout<<"Enter the  position to be deleted";
+	cin>>position;
+	create(n);
+	Delete(position);
+	display();
+}
+
+// Time Complexity is depend upon the position from where you want to delete
+
+// if position 1 or 2 than Time Complexity is O(1)
+
+// Best case is O(1) , Worst Case O(n)
