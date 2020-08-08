884ebeddf5249991b3e3aed9951710a79080777c
dhiman-12 <aayush.dhiman7777@gmail.com>
2020-08-08 10:54:09 +0530

    Stack

--- Stack/Stack-Operation-Using-Array.cpp
+++ Stack/Stack-Operation-Using-Array.cpp
@@ -1,3 +1,5 @@
+// Implement different operation on Stack using Array
+
 #include<iostream>
 using namespace std;
 struct stack{
@@ -9,49 +11,49 @@ struct stack{
 void create(struct stack *st)
 {
 	cout<<"Enter the size of stack";
-	cin>>st->size;
+	cin>>st->size;                   
 	st->top=-1;
-	st->s= new int [st->size];
+	st->s= new int [st->size];     // create size for stack in heap memeory
 }
 
 void push (struct stack *st,int x)
 {
-	if(st->top==st->size-1)
+	if(st->top==st->size-1)       // check stack is full or not 
 	{
 		cout<<"Stack Overflow"<<"\n";
 	}
 	else
 	{
-		st->top++;
-		st->s[st->top]=x;
+		st->top++;             // Increment the top value
+		st->s[st->top]=x;          // if stack is not full  than insert an element into the satck
 	}
 }
 
 int pop(struct stack *st)
 {
 	int x=-1;
-	if(st->top==-1)
+	if(st->top==-1)               // check stack is empty or not
 	{
 		cout<<"Stack Underflow"<<"\n";
 	}
 	else
 	{
-		x=st->s[st->top--];
+		x=st->s[st->top--];          // if not empty than than delete the top element 
 	}
-	return x;
+	return x;           // if stack is empty than return -1 otherwise return top element of the stack and decrement top also
 	
 }
 int peek(struct stack st, int index)
 {
 	int x=-1;
-	if(st.top-index+1<0)
+	if(st.top-index+1<0)        // check the element which we want is taht loaction is valid or not
 		cout<<"Position is Invalid";
 	else
-	x= st.s[st.top-index+1];	
+	x= st.s[st.top-index+1];	    // return the value at that index
 	return x;
 }
 
-int is_empty(struct stack st)
+int is_empty(struct stack st)    // check whether stack is empty or not, if empty return 1 else return 0
 {
 	if(st.top==-1)
 		return 1;
@@ -59,7 +61,7 @@ int is_empty(struct stack st)
 		return 0;	
 }
 
-int is_full(struct stack st)
+int is_full(struct stack st)     // check whether stack is full or not, if full return 1 else return 0
 {
 	if(st.size==st.top-1)
 		return 1;
@@ -67,17 +69,14 @@ int is_full(struct stack st)
 		return 0;
 }
 
-int stack_Top(struct stack st)
+int stack_Top(struct stack st)    // this function return the top value of the stack , if stack emoty than return -1
 {
 	if(st.top==-1)
 		return -1;
 	else
 		return st.s[st.top];
 }
-
-
-
-void display(struct stack *st)
+void display(struct stack *st)   // Display the element of the stack
 {
 	int i;
 	for(i=st->top;i>=0;i--)
@@ -102,7 +101,10 @@ int main()
 	cout<<is_empty(st)<<"\n";
 	cout<<is_full(st)<<"\n";
 	cout<<"Top Element is = "<<stack_Top(st)<<"\n";
-	display(&st);
-	
+	display(&st);	
 }
+
+// Time Complexity is constant i.e O(1) in every function except display function
+
+
 
